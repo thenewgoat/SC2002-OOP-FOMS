ace8957eab286ffc0806af47cb622e5e3470f1b8
Xinyang <zhangxinyang888@gmail.com>
2024-04-23 22:25:00 +0800

    Update FileDataService.java

--- foms/src/services/FileDataService.java
+++ foms/src/services/FileDataService.java
@@ -1,5 +1,38 @@
 package services;
 
+import java.util.Map;
+
+import models.Order;
+import models.User;
+
 public interface FileDataService {
+    
+    public Map<Integer, Order> importOrderData();
+
+    public boolean exportOrderData();
+
+    public Map<String, User> importUserData();
+
+    public boolean exportUserData();
+
+    public void importBranchUserData();
+
+    public void exportBranchUserData();
+
+    public void importMenuData();
+
+    public void exportMenuData();
+
+    public void importPaymentMethodData();
+
+    public void exportPaymentMethodData();
+
+    public void importBranchData();
+
+    public void exportBranchData();
+
+    public void importPasswordData();
+
+    public void exportPasswordData();
 
 }
