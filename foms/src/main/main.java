19cdebca9659eddd1932bb7a383ae67df5920a69
Xinyang <zhangxinyang888@gmail.com>
2024-04-24 19:12:55 +0800

    Main page

--- foms/src/main/main.java
+++ foms/src/main/main.java
@@ -1,12 +1,23 @@
 package main;
 
+import controllers.Welcome;
+import stores.UserStorage;
+import stores.*;
 import test.dataPersistenceTest;
 
 public class main {
     public static void main(String[] args) throws Exception {
 
-        dataPersistenceTest.test();
+        //dataPersistenceTest.test();
 
-        Welcome.welcome()
+        BranchUserStorage.load();
+        BranchStorage.load();
+        UserStorage.load();
+        OrderStorage.load();
+        PasswordStorage.load();
+        PaymentMethodStorage.load();
+        BranchMenuItemStorage.load();
+
+        Welcome.welcome();
     }
 }
