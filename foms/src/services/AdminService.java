84870f45c770af53326f1148c018b615eb5db37a
Xinyang <zhangxinyang888@gmail.com>
2024-04-26 19:27:44 +0800

    Update AdminService.java

--- foms/src/services/AdminService.java
+++ foms/src/services/AdminService.java
@@ -25,29 +25,56 @@ import utils.exceptions.TooFewManagersException;
 import utils.exceptions.TooManyManagersException;
 import enums.Gender;
 
+/**
+ * The AdminService class provides administrative functionalities related to the management of staff, branches, and payment methods for FOMS.
+ * 
+ * It implements the IAdminService interface, offering methods to add, remove, and update staff records, manage branch operations, and handle payment methods.
+ * 
+ * This class serves as a central point for administrative actions that affect the staffing and operational capabilities of branches, ensuring that business rules and constraints are adhered to during various management tasks.
+ */
 public class AdminService implements IAdminService{
 
     
-    
-
+    /**
+     * Retrieves a list of all staff members in the system.
+     * 
+     * @return an array of BranchUser objects representing all staff members in the system
+     */
     @Override
     public BranchUser[] getStaffList() {
-        // Return all staff members
         return BranchUserStorage.getAll();
     }
 
+    /**
+     * Retrieves a list of staff members in the system based on their age.
+     * 
+     * @param Age the age of the staff members to be retrieved
+     * @return an array of BranchUser objects representing staff members with the specified age
+     */
     public BranchUser[] getStaffList(int Age){
         return Arrays.stream(BranchUserStorage.getAll())
                      .filter(user -> user.getAge() == Age)
                      .toArray(BranchUser[]::new);
     }
 
+    /**
+     * Retrieves a list of staff members in the system based on their branch.
+     * 
+     * @param branch the branch of the staff members to be retrieved
+     * @return an array of BranchUser objects representing staff members in the specified branch
+     */
     public BranchUser[] getStaffList(Branch branch) {
         return Arrays.stream(BranchUserStorage.getAll())
                          .filter(user -> user.getBranchID() == branch.getID())
                          .toArray(BranchUser[]::new);
     }
 
+    /**
+     * Retrieves a list of staff members in the system based on their role.
+     * 
+     * @param role the role of the staff members to be retrieved
+     * @return an array of BranchUser objects representing staff members with the specified role
+     */
     public BranchUser[] getStaffList(Role role) {
         // Filter by role
         return Arrays.stream(BranchUserStorage.getAll())
@@ -55,6 +82,12 @@ public class AdminService implements IAdminService{
                      .toArray(BranchUser[]::new);
     }
 
+    /**
+     * Retrieves a list of staff members in the system based on their gender.
+     * 
+     * @param gender the gender of the staff to be retrieved
+     * @return an array of BranchUser objects representing staff members with the specified gender
+     */
     public BranchUser[] getStaffList(Gender gender) {
         // Filter by gender
         return Arrays.stream(BranchUserStorage.getAll())
@@ -62,10 +95,27 @@ public class AdminService implements IAdminService{
                      .toArray(BranchUser[]::new);
     }
 
+    /**
+     * Retrieves a list of all branches.
+     * 
+     * @return an array of Branch objects containing all branches.
+     */
     public Branch[] getBranchList(){
         return BranchStorage.getAll();
     };
 
+    
+    /**
+     * Adds a new staff member to the system.
+     * 
+     * The staff member is only added to the system if the staff quota for the branch is not exceeded and the branch has the required manager to staff ratio.
+     * 
+     * An account is also created for the new staff member with a default password of "password".
+     * 
+     * @param staff the staff member to be added
+     * @return true if the staff member was added successfully, false otherwise
+     * @throws IllegalArgumentException if the staff role is neither BRANCHMANAGER nor STAFF.
+     */
     @Override
     public boolean addStaff(BranchUser staff){
 
@@ -135,6 +185,16 @@ public class AdminService implements IAdminService{
         }
     };
 
+    /**
+     * Removes a staff member from the system.
+     * 
+     * Similar to addition, the staff member is only removed if the branch has the required manager to staff ratio after the removal.
+     * 
+     * The staff member's account is also deleted from the system.
+     * 
+     * @param staff the staff member to be removed
+     * @return true if the staff member was successfully removed, false otherwise
+     */
     @Override
     public boolean removeStaff(BranchUser staff){
         for (BranchUser branchUser : BranchUserStorage.getAll()){
@@ -187,6 +247,19 @@ public class AdminService implements IAdminService{
         return false;
     };
 
+
+    /**
+     * Updates the information of a staff member.
+     * 
+     * Only to be called when editing Name, Age and Gender.
+     * 
+     * Role and BranchID are not allowed to be changed without checks.
+     * 
+     * Staff ID is not allowed to be changed.
+     * 
+     * @param staff the BranchUser object representing the staff member to be updated
+     * @return true if the staff member was successfully updated, false otherwise
+     */
     @Override
     public boolean updateStaff(BranchUser staff){
         try {
@@ -197,6 +270,14 @@ public class AdminService implements IAdminService{
         }
     };
 
+    /**
+     * Promotes a staff member to a branch manager.
+     * 
+     * The promotion is only successful if the branch has the required manager to staff ratio after the promotion.
+     * 
+     * @param staff the staff member to be promoted
+     * @return true if the promotion is successful, false otherwise
+     */
     @Override
     public boolean promoteStaff(BranchUser staff){
 
@@ -236,6 +317,20 @@ public class AdminService implements IAdminService{
         }
     };
 
+
+    /**
+     * Transfers staff members from one branch to another.
+     * 
+     * The transfer is only successful if the staff members can be accommodated in the new branch without exceeding the staff quota and the required manager to staff ratio is maintained in both branches.
+     *
+     * Can support transferring one or multiple staff members at once.
+     * 
+     * @param staffList   an array of BranchUser objects representing the staff members to be transferred
+     * @param oldBranch   the branch from which the staff members are being transferred
+     * @param newBranch   the branch to which the staff members are being transferred
+     * @return true if the transfer is successful, false otherwise
+     * @throws IllegalArgumentException if the staff role is neither BRANCHMANAGER nor STAFF
+     */
     @Override
     public boolean transferStaff(BranchUser[] staffList, Branch oldBranch, Branch newBranch){
 
@@ -314,6 +409,14 @@ public class AdminService implements IAdminService{
         return true;
     }
 
+    /**
+     * Adds a new branch to the system.
+     * 
+     * Duplicate branch names are not allowed.
+     * 
+     * @param branch the branch to be added
+     * @return true if the branch was successfully added, false if the branch already exists
+     */
     @Override
     public boolean addBranch(Branch branch){
         for (Branch curBranch : BranchStorage.getAll()){
@@ -326,6 +429,16 @@ public class AdminService implements IAdminService{
         return true;
     };
 
+    /**
+     * Removes a branch from the system.
+     * This method removes the branch from the branch storage, deletes all associated orders and branch menu items.
+     * If the branch does not exist, it prints a message and returns false.
+     * 
+     * If branch still contains staff, it will send a warning before removing all staff from the branch and system.
+     *
+     * @param branch the branch to be removed
+     * @return true if the branch is successfully removed, false otherwise
+     */
     @Override
     public boolean removeBranch(Branch branch){
         for (BranchUser branchuser : getStaffList()){
@@ -355,6 +468,14 @@ public class AdminService implements IAdminService{
         return false;
     };
 
+    /**
+     * Adds a payment method to the system.
+     * 
+     * Duplicate payment names are not allowed, even across different payment types.
+     *
+     * @param paymentMethod the payment method to be added
+     * @return true if the payment method was successfully added, false otherwise
+     */
     @Override
     public boolean addPaymentMethod(PaymentMethod paymentMethod){
 
@@ -372,6 +493,13 @@ public class AdminService implements IAdminService{
         return true;
     };
 
+
+    /**
+     * Removes a payment method from the system.
+     * 
+     * @param paymentMethod the payment method to be removed
+     * @return true if the payment method was successfully removed, false otherwise
+     */
     @Override
     public boolean removePaymentMethod(PaymentMethod paymentMethod){
         for (PaymentMethod curPaymentMethod : PaymentMethodStorage.getAll()){
@@ -384,6 +512,13 @@ public class AdminService implements IAdminService{
         return false;
     }
 
+
+    /**
+     * Finds a payment method by its name.
+     *
+     * @param paymentMethod the name of the payment method to find
+     * @return the found PaymentMethod object, or null if not found
+     */
     public PaymentMethod findPaymentMethod(String paymentMethod){
         for (PaymentMethod curPaymentMethod : PaymentMethodStorage.getAll()){
             if (curPaymentMethod.getPaymentMethod().equals(paymentMethod)){
@@ -393,6 +528,12 @@ public class AdminService implements IAdminService{
         return null;
     }
 
+    /**
+     * Finds a staff member by their login ID.
+     *
+     * @param staffLoginID the login ID of the staff member to find
+     * @return the found BranchUser object, or null if not found
+     */
     public BranchUser findStaffByLoginID(String staffLoginID) {
         for (BranchUser staff : getStaffList()) {
             if (staff.getLoginID().equals(staffLoginID)) {
@@ -402,6 +543,12 @@ public class AdminService implements IAdminService{
         return null;
     }
 
+    /**
+     * Finds a branch by its ID.
+     *
+     * @param branchID the ID of the branch to find
+     * @return the found Branch object, or null if not found
+     */
     public Branch findBranchById(int branchID) {
         for (Branch branch : getBranchList()) {
             if (branch.getID() == branchID) {
@@ -411,6 +558,12 @@ public class AdminService implements IAdminService{
         return null;
     }
 
+    /**
+     * Finds an account by the loginID/staff ID.
+     *
+     * @param loginID the login ID of the account to find
+     * @return the found Account object, or null if not found
+     */
     public Account findAccountByLoginID(String loginID) {
         for (Account account : PasswordStorage.getAll()) {
             if (account.getLoginID().equals(loginID)) {
@@ -420,6 +573,16 @@ public class AdminService implements IAdminService{
         return null;
     }
 
+    /**
+     * Changes the password of a staff member.
+     * 
+     * @param user the staff member whose password is to be changed
+     * @param oldPassword the old password of the staff member
+     * @param newPassword the new password to be set
+     * @throws AccountNotFoundException if the account is not found
+     * @throws PasswordMismatchException if the old password provided does not match the account's current password
+     * @throws PasswordValidationException if the new password is invalid
+     */
     public void changePassword(User user, String oldPassword, String newPassword) throws AccountNotFoundException, PasswordMismatchException, PasswordValidationException {
         Account account = findAccountByLoginID(user.getLoginID());
     
@@ -437,6 +600,14 @@ public class AdminService implements IAdminService{
         PasswordStorage.update(account);
     }
 
+    /**
+     * Creates a new branch in the system.
+     * 
+     * @param branchName the name of the branch
+     * @param branchLocation the location of the branch
+     * @param staffQuota the maximum number of staff members that can be accommodated at the branch
+     * @return true if the branch was successfully created, false otherwise
+     */
     public boolean createBranch(String branchName, String branchLocation, int staffQuota) {
         Branch[] branches = getBranchList();
         int temp = 0;
@@ -449,10 +620,23 @@ public class AdminService implements IAdminService{
         return addBranch(branch);
     }
 
+    /**
+     * Retrieves all payment methods in the system.
+     * 
+     * @return an array of PaymentMethod objects representing all payment methods in the system
+     */
     public PaymentMethod[] getPaymentMethods() {
         return PaymentMethodStorage.getAll();
     }
 
+    
+    /**
+     * Creates a new account for a branch user.
+     * 
+     * @param staff the branch user for whom the account is being created
+     * @return true if the account is created successfully, false otherwise
+     * @throws IllegalArgumentException if the staff already exists
+     */
     private boolean createAccount(BranchUser staff){
         for (BranchUser curStaff: getStaffList()){
             if (curStaff.getLoginID().equals(staff.getLoginID())){
@@ -464,6 +648,12 @@ public class AdminService implements IAdminService{
         return true;
     }
 
+    /**
+     * Deletes an account from the system.
+     * 
+     * @param staff the branch user whose account is to be deleted
+     * @return true if the account is deleted successfully, false otherwise
+     */
     private boolean deleteAccount(BranchUser staff){
         for (BranchUser curStaff: getStaffList()){
             if (curStaff.getLoginID().equals(staff.getLoginID())){
