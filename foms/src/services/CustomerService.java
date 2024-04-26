fbe2a3bdb08aeaafd82c29c477182c6682ffeb33
Sai-Ajay <123815106+Sai-Ajay@users.noreply.github.com>
2024-04-26 12:38:36 +0000

    service javadocs done

--- foms/src/services/CustomerService.java
+++ foms/src/services/CustomerService.java
@@ -14,20 +14,40 @@ import stores.BranchStorage;
 import stores.OrderStorage;
 import stores.PaymentMethodStorage;
 
+/**
+ * The CustomerService class implements the ICustomerService interface and provides
+ * methods to interact with customer-related functionality in the system.
+ */
 public class CustomerService implements ICustomerService{
     
-
+    /**
+     * Retrieves the order with the specified OrderID.
+     *
+     * @param OrderID The ID of the order to retrieve.
+     * @return The Order object with the specified OrderID, or null if not found.
+     */
     @Override
     public Order getOrder(int OrderID) {
         Order order = OrderStorage.get(OrderID);
         return order;
     }
 
+    /**
+     * Creates a new order in the system.
+     *
+     * @param order The Order object to be created.
+     */
     @Override
     public void newOrder(Order order) {
         OrderStorage.add(order);
     }
 
+    /**
+     * Retrieves a list of branch menu items for the specified BranchID.
+     *
+     * @param BranchID The ID of the branch to retrieve menu items for.
+     * @return A list of BranchMenuItem objects for the specified BranchID, or null if not found.
+     */
     @Override
     public List<BranchMenuItem> getBranchMenuItemList(int BranchID) {
         BranchMenuItem[] items = BranchMenuItemStorage.getAll();
@@ -44,6 +64,12 @@ public class CustomerService implements ICustomerService{
         }
     }
 
+    /**
+     * Collects the order with the specified OrderID.
+     *
+     * @param OrderID The ID of the order to collect.
+     * @return true if the order was successfully collected, false otherwise.
+     */
     @Override
     public Boolean collectOrder(int OrderID) {
         Order order = OrderStorage.get(OrderID);
@@ -60,21 +86,44 @@ public class CustomerService implements ICustomerService{
         }
     }
 
+    /**
+     * Retrieves the next available OrderID.
+     *
+     * @return The next available OrderID.
+     */
     @Override
     public int getNextOrderID() {
         return OrderStorage.getAll().length + 1;
     }
 
+    /**
+     * Retrieves the name of the branch with the specified branchID.
+     *
+     * @param branchID The ID of the branch.
+     * @return The name of the branch with the specified branchID.
+     */
     @Override
     public String getBranchName(int branchID) {
         return BranchStorage.get(branchID).getName();
     }
 
+    /**
+     * Updates the specified branch menu item.
+     *
+     * @param item The BranchMenuItem object to update.
+     */
     @Override
     public void updateBranchMenuItem(BranchMenuItem item) {
         BranchMenuItemStorage.update(item);
     }
 
+    /**
+     * Retrieves the branch menu item with the specified branchID and itemName.
+     *
+     * @param branchID The ID of the branch.
+     * @param itemName The name of the menu item.
+     * @return The BranchMenuItem object with the specified branchID and itemName, or null if not found.
+     */
     public BranchMenuItem getBranchMenuItem(int branchID, String itemName){
         List<BranchMenuItem> items = getBranchMenuItemList(branchID);
         for (BranchMenuItem item : items) {
@@ -85,6 +134,12 @@ public class CustomerService implements ICustomerService{
         return null;
     }
 
+    /**
+     * Retrieves a list of payment methods of the specified type.
+     *
+     * @param type The type of payment method.
+     * @return A list of PaymentMethod objects of the specified type.
+     */
     public List<PaymentMethod> getPaymentMethods(String type) {
         List<PaymentMethod> paymentMethods = new ArrayList<>();
         PaymentMethod[] methods = PaymentMethodStorage.getAll();
@@ -96,11 +151,22 @@ public class CustomerService implements ICustomerService{
         return paymentMethods;
     }
 
+    /**
+     * Retrieves the ready time of the order with the specified OrderID.
+     *
+     * @param OrderID The ID of the order.
+     * @return The ready time of the order with the specified OrderID.
+     */
     public LocalDateTime getReadyTime(int OrderID) {
         Order order = OrderStorage.get(OrderID);
         return order.getReadyTime();
     }
 
+    /**
+     * Sets the status of the order with the specified OrderID to CANCELLED.
+     *
+     * @param OrderID The ID of the order.
+     */
     public void setOrderStatus(int OrderID) {
         Order order = OrderStorage.get(OrderID);
         order.setOrderStatus(OrderStatus.CANCELLED);
--- foms/src/services/StaffService.java
+++ foms/src/services/StaffService.java
@@ -14,8 +14,18 @@ import utils.exceptions.AccountNotFoundException;
 import utils.exceptions.PasswordMismatchException;
 import utils.exceptions.PasswordValidationException;
 
-public class StaffService implements IStaffService{
+/**
+ * The StaffService class implements the IStaffService interface and provides
+ * methods to perform various operations related to staff members.
+ */
+public class StaffService implements IStaffService {
 
+    /**
+     * Retrieves a list of orders associated with a specific branch.
+     *
+     * @param branchID the ID of the branch
+     * @return a list of orders associated with the branch, or null if no orders are found
+     */
     @Override
     public List<Order> getOrders(int branchID) {
         Order[] orders = OrderStorage.getAll();
@@ -31,17 +41,29 @@ public class StaffService implements IStaffService{
         return null;
     }
 
+    /**
+     * Retrieves an order by its ID.
+     *
+     * @param orderID the ID of the order
+     * @return the order with the specified ID, or null if no order is found
+     */
     @Override
     public Order getOrder(int orderID) {
         Order order = OrderStorage.get(orderID);
         return order;
     }
 
+    /**
+     * Updates the status of an order to "READY" if it is currently in the "PREPARING" state.
+     *
+     * @param orderID the ID of the order
+     * @return true if the order status is successfully updated, false otherwise
+     */
     @Override
     public Boolean updateOrderStatus(int orderID) {
         Order order = getOrder(orderID);
         if (order != null) {
-            if(order.getOrderStatus() == OrderStatus.PREPARING){
+            if (order.getOrderStatus() == OrderStatus.PREPARING) {
                 order.setOrderStatus(OrderStatus.READY);
                 return true;
             } else {
@@ -51,6 +73,12 @@ public class StaffService implements IStaffService{
         return false;
     }
 
+    /**
+     * Finds an account by its login ID.
+     *
+     * @param loginID the login ID of the account
+     * @return the account with the specified login ID, or null if no account is found
+     */
     @Override
     public Account findAccountByLoginID(String loginID) {
         for (Account account : PasswordStorage.getAll()) {
@@ -61,10 +89,20 @@ public class StaffService implements IStaffService{
         return null;
     }
 
+    /**
+     * Changes the password of a user.
+     *
+     * @param user         the user whose password needs to be changed
+     * @param oldPassword  the old password
+     * @param newPassword  the new password
+     * @throws AccountNotFoundException     if no account is found with the specified login ID
+     * @throws PasswordMismatchException     if the old password provided is incorrect
+     * @throws PasswordValidationException  if the new password is invalid
+     */
     @Override
     public void changePassword(User user, String oldPassword, String newPassword) throws AccountNotFoundException, PasswordMismatchException, PasswordValidationException {
         Account account = findAccountByLoginID(user.getLoginID());
-    
+
         if (account == null) {
             throw new AccountNotFoundException("No account found with login ID: " + user.getLoginID());
         }
@@ -74,11 +112,16 @@ public class StaffService implements IStaffService{
         if (newPassword == null || newPassword.isEmpty() || newPassword.equals("password")) {
             throw new PasswordValidationException("The new password is invalid. It cannot be null, empty, or 'password'.");
         }
-    
+
         account.setPassword(newPassword);
         PasswordStorage.update(account);
     }
 
+    /**
+     * Cancels an order by setting its status to "CANCELLED".
+     *
+     * @param orderID the ID of the order to be cancelled
+     */
     @Override
     public void cancelOrder(int orderID) {
         Order order = getOrder(orderID);
