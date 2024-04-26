ec7b85a52a12adffb6b52d377a6095953b089b81
Xinyang <zhangxinyang888@gmail.com>
2024-04-26 19:09:41 +0800

    Update FomsApp.java

--- foms/src/main/FomsApp.java
+++ foms/src/main/FomsApp.java
@@ -7,6 +7,19 @@ import controllers.Welcome;
  * This class contains the entry point of the application.
  */
 public class FomsApp {
+
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private FomsApp() {
+    }
+
+    /**
+     * The entry point of the application.
+     *
+     * @param args the command line arguments
+     * @throws Exception Throws an exception if there are issues during the execution, such as input/output errors.
+     */
     public static void main(String[] args) throws Exception {
 
         Welcome.welcome();
